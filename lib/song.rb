01c62b5aef4db1f8bb7392e39280e94ce29f4adf
bella13 <isabellad.13@icloud.com>
2018-07-13 18:54:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,7 @@ class Song
  def initialize(name, artist, genre)
    @name = name 
    @artist = artist 
-   @genres = genre 
+   @genre = genre 
    @@count += 1 
    @@genres << genre 
    @@artists << artist 
